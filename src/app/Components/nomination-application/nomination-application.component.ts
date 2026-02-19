a49631597f01ec3127346ef5db98326251e15e1b
Dev Shifa <shifa.alam@lucidtech.com.bd>
2026-02-19 17:10:49 +0600

    updated component

--- src/app/Components/nomination-application/nomination-application.component.ts
+++ src/app/Components/nomination-application/nomination-application.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
 import { Candidate, CandidateService } from '../../Services/candidate.service';
 import { Position, PositionService } from '../../Services/position.service';
 import { SnackbarService } from '../../Services/snackbar.service';
-import { CandidateAddComponent } from '../candidate-add/candidate-add.component';
 import { MatProgressBarModule } from "@angular/material/progress-bar";
-import { MatIconModule } from "@angular/material/icon";
 import { MatSelectModule } from "@angular/material/select";
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-
+import { Member, MemberService } from '../../Services/member.service';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-nomination-application',
   standalone: true,
@@ -29,6 +27,8 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './nomination-application.component.scss'
 })
 export class NominationApplicationComponent implements OnInit {
+  member: Member | undefined;
+  profileImageUrl: string | undefined;
   showPopup = false;
   candidate: Candidate = {
     id: 0,
@@ -47,19 +47,48 @@ export class NominationApplicationComponent implements OnInit {
   positions: Position[] = [];
   // Initialize supporter array
   supporters = [
-    { name: '', batch: '', memberId: '' },
-    { name: '', batch: '', memberId: '' },
-    { name: '', batch: '', memberId: '' }
+    { phone: '', memberId: '' },
+    { phone: '', memberId: '' },
+    { phone: '', memberId: '' }
   ];
+
+
+  candidateName: string = '';
+  postName: string = '';
+  updateMessage: string = '';
+  commissionerName: string = 'প্রধান নির্বাচন কমিশনার, UPSAA';
+
+  importantDates: string[] = [
+    '২৮ ফেব্রুয়ারি ২০২৬: প্রার্থীতা চূড়ান্ত',
+    '১লা মার্চ ২০২৬: ব্যালট নাম্বার প্রকাশ',
+    '১৩ মার্চ ২০২৬: ভোট গ্রহণ ও ফল প্রকাশ'
+  ];
+  selectedPositionName: string = '';
+
   constructor(
 
     private candidateService: CandidateService,
     private snackbarService: SnackbarService,
     private positionService: PositionService,
+    private memberService: MemberService,
 
   ) { }
   ngOnInit(): void {
     this.loadPositions();
+    this.loadProfile();
+  }
+  loadProfile() {
+    this.loading = true
+    this.memberService.getProfile().subscribe({
+      next: res => {
+        this.profileImageUrl = res?.photo ? res.photo + '?t=' + new Date().getTime() : '';
+        this.member = res;
+      },
+      error: () => {
+        this.profileImageUrl = '';
+      },
+      complete: () => this.loading = false
+    });
   }
   loadPositions() {
     this.positionService.getPositions().subscribe({
@@ -70,19 +99,22 @@ export class NominationApplicationComponent implements OnInit {
   // Call this before submit
   updateApplicationReason() {
     // Filter out empty supporters
-    const filled = this.supporters.filter(s => s.name && s.batch && s.memberId);
+    const filled = this.supporters.filter(s => s.phone && s.memberId);
 
     // Join them into a single string
     this.candidate.applicationReason = filled
-      .map(s => `${s.name}-${s.batch}-${s.memberId}`)
+      .map(s => `${s.phone}-${s.memberId}`)
       .join(', ');
   }
   addSupporter() {
     if (this.supporters.length < 5) {  // max 5 supporters
-      this.supporters.push({ name: '', batch: '', memberId: '' });
+      this.supporters.push({ phone: '', memberId: '' });
     }
   }
-  submitNomination() {
+  onPositionChange(selectedId: any) {
+    const pos = this.positions.find(p => p.id == selectedId);
+    this.selectedPositionName = pos ? pos.name : '';
+  } submitNomination() {
     if (!this.candidate.positionId) {
 
       this.snackbarService.showError('Please select a position.');
@@ -92,11 +124,12 @@ export class NominationApplicationComponent implements OnInit {
 
 
     this.loading = true;
-    console.log(this.candidate);
+
     this.candidateService.applyNomination(this.candidate).subscribe({
       next: () => {
-
-        this.snackbarService.showSuccess('Nomination submitted successfully 🎉');
+        this.showPopup = true;
+        // this.snackbarService.showSuccess('Nomination submitted successfully 🎉');
+        this.downloadJPG()
         this.loading = false;
       },
       error: (err) => {
@@ -105,10 +138,27 @@ export class NominationApplicationComponent implements OnInit {
       },
 
     });
+
   }
+
+
 
   closePopup() {
-
+    this.showPopup = false;
   }
 
+  downloadJPG() {
+
+    setTimeout(() => {
+      const card = document.getElementById('greetingCard');
+      if (!card) return;
+
+      html2canvas(card, { scale: 4 }).then(canvas => {
+        const link = document.createElement('a');
+        link.download = `${this.member?.fullName}-nomination.jpg`;
+        link.href = canvas.toDataURL('image/jpeg', 1.0);
+        link.click();
+      });
+    }, 200);
+  }
 }
