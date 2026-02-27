3d3f32437cba88aefbd3e5d7175453bac1925d89
Dev Shifa <shifa.alam@lucidtech.com.bd>
2026-02-27 16:53:13 +0600

    image updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   //baseUrl: 'https://eprod.kghdhaka.online/api' // Your backend API URL
-  baseUrl: 'https://etest.kghdhaka.online/api' // Your backend API URL
-  //baseUrl: 'http://localhost:5219/api'// Your backend API URL
+  //baseUrl: 'https://etest.kghdhaka.online/api' // Your backend API URL
+  baseUrl: 'http://localhost:5219/api'// Your backend API URL
+  // baseUrl: 'https://sbfqa.lucidtech.website/api'// Your backend API URL
+
 };
