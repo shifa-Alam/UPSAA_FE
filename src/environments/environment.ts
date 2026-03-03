d9d5549406581ba6f084ea0f7db2b47c381bea18
Shifa Alam <shifa7446@gmail.com>
2026-03-03 23:27:57 +0600

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   //baseUrl: 'https://etest.kghdhaka.online/api' // Your backend API URL
   //baseUrl: 'http://localhost:5219/api'// Your backend API URL
    //baseUrl: 'https://sbfqa.lucidtech.website/api'// Your backend API URL
-   //baseUrl: 'https://shsmchuat.lucidtech.website/api'// Your backend API URL
- baseUrl: 'https://localhost:7287/api'// Your backend API URL
+   baseUrl: 'https://shsmchuat.lucidtech.website/api'// Your backend API URL
+ //baseUrl: 'https://localhost:7287/api'// Your backend API URL
 
 };
